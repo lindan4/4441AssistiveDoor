562ed2dd308b45ed07ca66989aeba846544e3687
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-03-24 17:58:27 -0400

    got the sqlite db working with dynnamic id reitreval. need to give the user the option to delete their files now.

--- datasetCreator.py
+++ datasetCreator.py
@@ -64,12 +64,12 @@ def creationMechanism():
                 faces=faceDetect.detectMultiScale(gray,1.3,5);
                 for(x,y,w,h) in faces:
                     sampleNum=sampleNum+1;
-                    cv2.imwrite("facesDataTest/"+str(name.get())+"."+ str(serialID)+"." +str(sampleNum)+ ".jpg", gray[y:y+h, x:x+w])
+                    cv2.imwrite("facesData/"+str(name.get())+"."+ str(serialID)+"." +str(sampleNum)+ ".jpg", gray[y:y+h, x:x+w])
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
                     cv2.waitKey(100);
                 cv2.imshow("face",img);
                 cv2.waitKey(1)
-                if(sampleNum>20):
+                if(sampleNum>100):
                     break
                 
             cam.release()
--- detector.py
+++ detector.py
@@ -1,10 +1,29 @@
 import cv2
 import numpy as np
+import sqlite3
 
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
 cam=cv2.VideoCapture(1);
 rec=cv2.face.LBPHFaceRecognizer_create();
 rec.read("recognizer\\traningData.yml")
+
+def tableInsert(Id):
+        name=22
+        conn=sqlite3.connect('FaceBase.db')
+        print "connected successfully"
+        cur=conn.execute("Select Name from People where SerialNumber = ?", [int(Id)])
+        conn.commit()
+
+        #cur=conn.cursor()
+        
+        row=cur.fetchone()
+
+        for r in row:
+            name=r
+            
+        print(str(name))
+        conn.close()
+        return name
 
 
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
@@ -15,17 +34,17 @@ while(True):
     for(x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         Id,conf=rec.predict(gray[y:y+h,x:x+w])
-        if(conf<55):    # Play around with these values because camera is shitty
-            if(Id==1):
-                Id="Lindan"
-#            elif(Id==2):
-###                Id="Sam"
-        else:
-            Id="Unknown"
-        cv2.putText(img,str(Id),(x,y+h),font,3.0,(0,255,0));
         print(conf);
+        if(conf<44):    # Play around with these values because camera is shitty
+           print("confidence below 42")
+           name=tableInsert(Id)
+        else:
+            name="Unknown"
+        cv2.putText(img,str(name),(x,y+h),font,3.0,(0,255,0));
+        
     cv2.imshow("face",img);
     if(cv2.waitKey(1)==ord('q')):
         break;
 cam.release()
 cv2.destroyAllWindows()
+
